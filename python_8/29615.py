2777565a13c28ab1ce60368aebf38db6754e2d60
kengo hirano <daeyangkim124@gmail.com>
2024-03-25 23:36:56 +0900

    24.03.25.commit

---
+++ python_8/29615.py
@@ -0,0 +1,22 @@
+from typing import *
+import sys
+
+input = sys.stdin.readline
+
+N , M = map(int , input().split())
+arr = list(map(int , input().split()))
+friends = list(map(int , input().split()))
+
+idx = 0
+count = 0
+for f in friends:
+    for i in range(len(arr)):
+        if arr[i] == f:
+            if i == idx:
+                idx += 1
+            else:
+                arr[i] , arr[idx] = arr[idx] , arr[i]
+                count += 1
+                idx += 1
+
+print(count)
