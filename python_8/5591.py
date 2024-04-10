7fa1d86338301e862f9bce3e423a7813bee5d678
kengo hirano <daeyangkim124@gmail.com>
2024-04-10 22:01:56 +0900

    24.04.10.commit

--- python_8/5591.py
+++ python_8/5591.py
@@ -6,10 +6,13 @@ input = sys.stdin.readline
 N , K = map(int , input().split())
 arr = [int(input()) for _ in range(N)]
 
-length = N - K + 1
+prefix_sum = [0] * (N + 1)
+for i in range(N):
+    prefix_sum[i + 1] = arr[i] + prefix_sum[i]
 
-result = []
-for idx in range(length):
-    result.append(sum(arr[idx : idx + K]))
+maxx : int = 0
+for idx in range(K , N + 1):
+    num : int = prefix_sum[idx] - prefix_sum[idx - K]
+    if num > maxx: maxx = num
 
-print(max(result))
+print(maxx)
